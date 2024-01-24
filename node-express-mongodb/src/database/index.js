ddc9f963581dc79ee635e6ff04f08ac032e34b70
fernndes <ferndes.gabriel@hotmail.com>
2024-01-24 17:29:26 -0300

    feat: change mongoose connection

--- node-express-mongodb/src/database/index.js
+++ node-express-mongodb/src/database/index.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose")
 
-async function main() {
-    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority`)
-}
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority`)
 
-main().catch(err => console.log(err));
+module.exports = mongoose
