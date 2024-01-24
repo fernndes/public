e3a18193414c12e2d1af5614cc389e9f0782dd4f
Gabriel Fernandes <ferndes.gabriel@hotmail.com>
2024-01-24 18:44:35 -0300

    feat: add dotenv

--- node-express-mongodb/src/index.js
+++ node-express-mongodb/src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3333
 
+require('dotenv').config()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
