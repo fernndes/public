1644b4120ba0ff6a0eb49a559eb0baf10ee6062c
fernndes <ferndes.gabriel@hotmail.com>
2024-02-04 13:17:56 -0300

    feat: config app router

--- google-oauth2-node/src/app.ts
+++ google-oauth2-node/src/app.ts
@@ -1,14 +1,59 @@
-import express, { Request, Response } from 'express'
-const PORT = 8000
+require("dotenv").config();
+import path from "path";
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route";
+import authRouter from "./routes/auth.route";
+import sessionRouter from "./routes/session.route";
+import connectDB from "./utils/prisma";
 
-const app = express()
+const app = express();
 
-app.get("/api/healthChecker", (req: Request, res: Response) => {
-    res.status(200).json({
-      status: "success",
-      message: "Implement Google OAuth2 in Node.js",
-    });
+app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+app.use("/api/images", express.static(path.join(__dirname, "../public")));
+
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN as unknown as string;
+app.use(
+  cors({
+    credentials: true,
+    origin: [FRONTEND_ORIGIN],
+  })
+);
+
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/sessions", sessionRouter);
+
+app.get("/api/healthChecker", (_: Request, res: Response) => {
+  res.status(200).json({
+    status: "success",
+    message: "Implement OAuth in Node.js",
   });
+});
 
+// UnKnown Routes
+app.all("*", (req: Request, _: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+app.use((err: any, _: Request, res: Response, __: NextFunction) => {
+  err.status = err.status || "error";
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
+const port = process.env.SERVER_PORT;
+app.listen(port, () => {
+  console.log(`✅ Server running on port: ${port}`);
+  connectDB();
+});
